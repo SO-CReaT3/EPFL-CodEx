fadebda9e82069fcbda04b1c6c5fd2f3db338255
AkimBerreqia <berreqia.akim@gmail.com>
2026-02-21 19:11:37 +0100

    Update it works and Exercice 1 done

---
+++ BA 2/PPOO/Serie_1/TSTU/src/cs108/BoundedIntQueueOk.java
@@ -0,0 +1,10 @@
+package cs108;
+
+public class BoundedIntQueueOk extends BoundedIntQueue {
+    private 
+    private int capacity;
+
+    public BoundedIntQueueOk(int capacity) {
+
+    }
+}
--- BA 2/PPOO/Serie_1/TSTU/test/cs108/BoundedIntQueueTest.java
+++ BA 2/PPOO/Serie_1/TSTU/test/cs108/BoundedIntQueueTest.java
@@ -3,8 +3,59 @@ package cs108;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class BoundedIntQueueTest {
+
     @Test
-    void boundedIntQueue
+    void BoundedIntQueueNegativeCapacityIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new BoundedIntQueueBuggy(-1);
+        });
+    }
+
+    @Test
+    void capacity() {
+        BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(10);
+        assertEquals(10, queue.capacity());
+    }
+
+    @Test
+    void size() {
+        BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(10);
+        assertEquals(0, queue.size());
+    }
+
+    @Test
+    void isEmpty() {
+        BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(0);
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    void isFull() {
+        BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(3);
+        assertFalse(queue.isFull());
+
+        queue.addLast(1);
+        queue.addLast(2);
+        queue.addLast(3);
+        assertTrue(queue.isFull());
+    }
+
+    @Test
+    void addLast() {
+        assertThrows(java.lang.IllegalStateException.class, () -> {
+            BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(2);
+            queue.addLast(1);
+            queue.addLast(2);
+            queue.addLast(3);
+        });
+    }
+
+    @Test
+    void removeFirst() {
+        assertThrows(java.lang.IllegalStateException.class, () -> {
+            BoundedIntQueueBuggy queue = new BoundedIntQueueBuggy(2);
+            queue.removeFirst();
+        });
+    }
 }
